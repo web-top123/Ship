906717fb6e76ccb284af36aa79bbb51b5fc3d0f2
web-top123 <webtopc2021@gmail.com>
2023-01-24 22:03:53 +0900

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
 import { configureStore } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
