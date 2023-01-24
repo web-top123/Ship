a4fd19f4acd2bd31aa17a9765e657ff9eb266cc7
web-top123 <webtopc2021@gmail.com>
2023-01-24 23:16:35 +0900

    Revert "test"
    This reverts commit 906717fb6e76ccb284af36aa79bbb51b5fc3d0f2.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+
 import { configureStore } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
