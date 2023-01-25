f808e535b2445b4f13a48252cd6a363681237862
web-top123 <webtopc2021@gmail.com>
2023-01-25 12:07:47 +0900

    my information update

--- src/Components/Common/MineSidebar.js
+++ src/Components/Common/MineSidebar.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import classnames from "classnames";
-import { Card, CardBody, Col, Container, Nav, NavItem, NavLink, Row, TabContent, TabPane } from "reactstrap";
+import { Label, CardBody, Col, Input, Nav, NavItem, NavLink, Row, TabContent, TabPane } from "reactstrap";
+import { FormGrid, Gutters, VerticalForm, HorizontalForm, HorizontalFormLabelSizing, ColumnSizing, AutoSizing, InlineForms, FloatingLabels } from '../../pages/Forms/FormLayouts/FormlayoutsCode';
+
+import Flatpickr from "react-flatpickr";
+
 const MineSidebar = () => {
     // Vertical Nav Tabs
     const [verticalTab, setverticalTab] = useState("1");
@@ -140,16 +144,71 @@ const MineSidebar = () => {
                             id="v-pills-tabContent"
                         >
                             <TabPane tabId="1" id="v-pills-home">
-                                <div className="d-flex mb-2">
-                                    <div className="flex-shrink-0">
+                            <CardBody className='pt-0'>
+                                    <div className="live-preview">
+                                        <form action="#">
+                                            <Row className="mb-3">
+                                                <Col lg={3} >
+                                                    <Label htmlFor="identifierInput" className="form-label">Identifier</Label>
+                                                </Col>
+                                                <Col lg={9} >
+                                                    <Input type="text" className="form-control" id="identifierInput" placeholder="Enter your Identififer" />
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-3">
+                                                <Col lg={3} >
+                                                    <Label htmlFor="leaveemails" className="form-label">Email Address</Label>
+                                                </Col>
+                                                <Col lg={9} >
+                                                    <Input type="email" className="form-control" id="leaveemails" placeholder="Enter your email" />
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-3">
+                                                <Col lg={3} >
+                                                    <Label htmlFor="nameinput" className="form-label">Name</Label>
+                                                </Col>
+                                                <Col lg={9} >
+                                                    <Input type="url" className="form-control" id="username" placeholder="Enter your url" />
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-3">
+                                                <Col lg={3} >
+                                                    <Label htmlFor="mankind" className="form-label">Man / Woman</Label>
+                                                </Col>
+                                                <Col lg={9} >
+                                                    <select type="number" className="form-select" id="mankind" >
+                                                        <option value="man" selected>Man</option>
+                                                        <option value="woman">Woman</option>    
+                                                    </select>
+                                                </Col>
+                                            </Row>
+                                            <Row className="mb-3">
+                                                <Col lg={3} >
+                                                    <Label htmlFor="birthday" className="form-label">Birthday</Label>
+                                                </Col>
+                                                <Col lg={9} >
+                                                <Flatpickr
+                                                    className="form-control"
+                                                    options={{
+                                                        dateFormat: "d M, Y"
+                                                    }}
+                                                />
+                                                </Col>
+                                            </Row>
+                                            
+                                            <div className="text-end">
+                                                <button type="submit" className="btn btn-primary">Save</button>
+                                            </div>
+                                        </form>
                                     </div>
-                                    <div className="flex-grow-1">
-                                        <p className="mb-0">You always want to make sure that your fonts work well together and try to limit the number of fonts you use to three or less. Experiment and play around with the fonts that you already have in the software you’re working with reputable font websites.</p>
+                                    <div className="d-none code-view">
+                                        <pre className="language-markup" style={{ "height": "375px" }}>
+                                            <code>
+                                                <HorizontalForm />
+                                            </code>
+                                        </pre>
                                     </div>
-                                </div>
-                                <p className="mb-0">
-                                    This may be the most commonly encountered tip I received from the designers I spoke with. They highly encourage that you use different fonts in one design, but do not over-exaggerate and go overboard.
-                                </p>
+                                </CardBody>
                             </TabPane>
                             <TabPane tabId="2" id="v-pills-profile">
                                 <div className="d-flex mb-2">
