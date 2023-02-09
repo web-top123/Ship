40ffcfbce9aa6e5c2e7b92b04ec40a3768ee94e9
paramountsky7 <paramountsky7@gmail.com>
2023-02-09 15:40:33 +1100

    Create config - sample.js

---
+++ src/config - sample.js
@@ -0,0 +1,13 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+
+  //API_URL:"http://10.10.12.77:8080/"
+  API_URL:"http://localhost:8080/"
+}
