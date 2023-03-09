81c6d627460d06785745a0201c3360c460d0803d
webile707 <vm777000888@gmail.com>
2023-03-09 13:33:33 +0300

    changed init url of login

--- src/store/auth/login/saga.js
+++ src/store/auth/login/saga.js
@@ -41,7 +41,7 @@ function* loginUser({ payload: { user, history } }) {
       localStorage.setItem("authUser", JSON.stringify(response));
       yield put(loginSuccess(response));
     }
-    history.push("/pages-profile-settings");
+    history.push("/pages-home-page");
   } catch (error) {
     yield put(apiError(error));
   }
