b22042288ca967c90e11262cd5753f4888e2df45
superspider579 <121188599+jenisHan@users.noreply.github.com>
2023-03-14 12:59:35 +0900

    delete getTraceViews api in fakebackend

--- src/helpers/url_helper.js
+++ src/helpers/url_helper.js
@@ -311,4 +311,3 @@ export const ADD_NEW_DATA_PURCHASE_HISTORY = "/api/datapurchasehistory/create";
 export const GET_DATA_PURCHASE_HISTORY_ALL = "/api/dataPurchaseHistory/get_AllByVoterId";
 // DataPurchaseHistory
 export const ADD_NEW_TEST_PURCHASE_HISTORY = "/api/testPurchaseHistory/create";
-export const GET_DATA_PURCHASE_HISTORY_ALL = "/api/dataPurchaseHistory/get_AllByVoterId";
